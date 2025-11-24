03b61f14d0bf1e3baacb1f28fef8d7a637ba2a40
et1812cda48 <72542297+et1812cda48@users.noreply.github.com>
2025-11-24 18:07:49 +0500

    second_with_alarm

--- app.py
+++ app.py
@@ -1,33 +1,41 @@
-from datetime import time
+from datetime import datetime
 from ultralytics import YOLO
 import numpy as np
 import cv2
 import os
 
+alarm_flag = False
+
 def build_security_zone(frame, border_zones):
-    cv2.polylines(frame, border_zones, isClosed=True, color=(0, 0, 255), thickness=2)
+    cv2.polylines(frame, border_zones, isClosed=True, color=(5, 0, 255), thickness=2)
     return frame
 
 def person_in_zone(xyxy, zone):
     x1, y1, x2, y2 = xyxy
-    left, top, bottom, right = zone[0, 0], zone[0, 1], zone[2, 1], zone[2, 0]
-    if ((x1 > left and x1 < right or x2 > left and x2 < right) and
-            (y1 > bottom and y1 < top or y2 > bottom and y2 < top)):
-        return True
-    return False
+    x, y = (x1 + x2) / 2, (y1 + y2) / 2
+    intersects = cv2.pointPolygonTest(zone, (x, y), False)
+    return intersects
 
 def person_processing(frame, box_coords, border_zones):
+    global alarm_flag
+    timer = 0
     x1, y1, x2, y2 = box_coords
     x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
     cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
     for zone in border_zones:
         if person_in_zone([x1, y1, x2, y2], zone):
-            cv2.putText(frame, 'Alarm!!!', (frame.shape[0] // 2 - 5, frame.shape[1] // 2 - 3),
-                        cv2.FONT_ITALIC, 24, (0, 0, 255), 2)
+            alarm_flag = True
+            timer = datetime.now()
+        if alarm_flag:
+            cv2.putText(frame, 'Alarm!!!', (frame.shape[1] // 2 - 20, frame.shape[0] // 2 - 3),
+                        cv2.FONT_HERSHEY_SIMPLEX, 4, (0, 0, 255), 2)
+            break
+    if (datetime.now() - timer).total_seconds() / 60 >= 3: alarm_flag = False
     return frame
 
 video_path = 'test.mp4'
 frame_folder = 'frames'
+model_folder = 'models'
 
 coords = []
 with open('security_zone_coords.txt', 'r') as file:
@@ -37,12 +45,12 @@ with open('security_zone_coords.txt', 'r') as file:
         coords.append([x, y])
 security_coords = [np.array(coords[i:i+4]) for i in range(0, len(coords), 4)]
 
-model = YOLO('yolo11n.pt')
+model = YOLO(f'{model_folder}/yolo11n.pt')
 cap = cv2.VideoCapture(video_path)
-#writer = cv2.VideoWriter('detected_video.mp4', cv2.VideoWriter_fourcc(*'MP4V'), 20.0, (720, 1280))
+writer = cv2.VideoWriter('detected_video.avi', cv2.VideoWriter.fourcc(*'XVID'), 20.0, (720, 1280))
 if not os.path.exists(frame_folder):
     os.mkdir(frame_folder)
-num = 1
+
 while True:
     success, frame = cap.read()
     if not success: break
@@ -55,8 +63,6 @@ while True:
     for box in result.boxes:
         if box.cls[0] != 0: continue
         frame = person_processing(frame, box.xyxy[0], security_coords)
-    cv2.imwrite(f'{frame_folder}/frame_{num}.jpg', frame)
-    #writer.write(frame)
-    num += 1
+    writer.write(frame)
 cap.release()
-#writer.release()
+writer.release()
